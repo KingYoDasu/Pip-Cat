35c2a37d4d04901e1c66d69751e46be383ce0159
SakanaIsReal <50800340+KingYoDasu@users.noreply.github.com>
2022-04-18 01:29:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,6 @@ const popcat = document.querySelector("#popcat");
 const btn = document.querySelector("#btn");
 const span = document.querySelector("span");
 const embed = new Discord.RichEmbed()
-.setTitle("มีไอ้ไปร์ทร้อน ๆ ให้กดในบริเวณ 100 ม. ใกล้คุณ!")
-.setURL("https://pipcat.netlify.app/")
-.setAuthor("Pip-Cat | บริการถึงใจ ฉับไวถึงคุณ", "https://media.discordapp.net/attachments/527264094622056449/965242969554812948/received_466274227391041.jpeg?width=587&height=702","https://pipcat.netlify.app/")
-.setColor(#ff0000)
-.setDescription("กดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบกดหน่อยคัรบ")
-.setFooter("Provide to you by @SukiKana4826, https://media.discordapp.net/attachments/527264094622056449/965242969554812948/received_466274227391041.jpeg?width=587&height=702")
-.setImage("https://media.discordapp.net/attachments/527264094622056449/965242969554812948/received_466274227391041.jpeg?width=587&height=702")message.channel.send({embed})
 
 // The two images of the POP CAT
 const openMouthImg = "./images/pip.jpeg";
