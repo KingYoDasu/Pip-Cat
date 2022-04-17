330770bd9d33b30eb7076420e8580fdadbd3f18e
SakanaIsReal <50800340+KingYoDasu@users.noreply.github.com>
2022-04-17 22:26:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// References to DOM elements
+const popcat = document.querySelector("#popcat");
+const btn = document.querySelector("#btn");
+const span = document.querySelector("span");
+
+// The two images of the POP CAT
+const openMouthImg = "./images/pip.jpeg";
+
+// The two Popping sounds
+const openMouthSound = new Audio("./sound/pip.wav");
+
+// Event Handlers (Desktops)
+btn.addEventListener("mousedown", openMouth);
+btn.addEventListener("mousedown", increment)
+
+// Event Handers (Touch Screens)
+btn.addEventListener("touchstart", function (e) {
+  e.preventDefault();
+  openMouth();
+});
+
+// The functions which will perform the cool stuff
+function openMouth() {
+  popcat.src = openMouthImg;
+  openMouthSound.play();
+}
+
+function increment() {
+    span.innerHTML++;
+}
